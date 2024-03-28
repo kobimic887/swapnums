aebcfdbce3c29b0920d9007ca0fe14c53c52cda1
kobimic887 <60321797+kobimic887@users.noreply.github.com>
2024-03-28 14:44:04 +0200

    Add project files.

---
+++ ConsoleApplication16.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+int getLastDigit(int num)
+{
+    int l = 0;
+    ///
+    return l;
+}
+
+int getDigitSum(int num)
+{
+    int sum = 0;
+
+    ///
+    return sum;
+}
+
+void getRes(int* p_num)
+{
+    int sum = 0;
+    ///
+
+}
+int main()
+{
+    int num = 0, orgNum = 0;
+    printf("Please enter youir number: \n");
+    scanf_s("%d", &num);
+    orgNum = num;
+    getRes(&num);
+
+    printf(" Original number is %d -> %d", orgNum, num);
+
+}
