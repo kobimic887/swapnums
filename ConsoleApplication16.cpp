734fe1d0a7b41e1fd13f4bc82ab888decb13559a
kobimic887 <60321797+kobimic887@users.noreply.github.com>
2024-04-04 14:39:28 +0300

    comm

--- ConsoleApplication16.cpp
+++ ConsoleApplication16.cpp
@@ -2,22 +2,22 @@
 int getLastDigit(int num)
 {
     int l = 0;
-    ///
+    l = num % 10;
     return l;
 }
 
 int getDigitSum(int num)
 {
     int sum = 0;
-
-    ///
+    sum += l;
+    
     return sum;
 }
 
 void getRes(int* p_num)
 {
     int sum = 0;
-    ///
+    
 
 }
 int main()
